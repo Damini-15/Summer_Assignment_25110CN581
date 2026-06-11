bf61dddfb218cc44b7b5b6642a85d718abda0545
Damini-06 <25110cn581@glbitm.ac.in>
2026-06-11 14:02:33 +0530

    Added day9 programs

---
+++ day_9/program34.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ day_9/program35.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        for(int i=65;i<=69;i++)
+        {
+            for(int j=65;j<=i;j++)
+            {
+                System.out.print((char)i);
+            }
+            System.out.println("");
+        }
+    }
+}
